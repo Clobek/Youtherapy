954616214ab1897ef07173ffff7b0aac3effd24f
Bryce <blizzardman1202@hotmail.com>
2020-08-06 06:52:45 -0500

    additional support added for tablets, nav bar complete, home page in progress

--- pages/index.js
+++ pages/index.js
@@ -9,34 +9,62 @@ export default function Home() {
       <Head>
         <title>Youtherapy</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"></link>
         <script src="https://kit.fontawesome.com/8187678fb5.js" crossorigin="anonymous" defer></script>
       </Head>
       <header>
-        <div className="logo"></div>
+      <Link href="/"><div className="logo"></div></Link>
         <div className="nav">
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">About</a></li>
+            <li><Link href="/"><a>Home</a></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
             <li className="dropdown">
-              <div className="dropbtn">Services&nbsp;<i className="fas fa-caret-down"></i></div>
+              <div className="dropbtn">Services&nbsp;<i aria-hidden className="fas fa-caret-down"></i><i aria-hidden className="fas fa-caret-up"></i></div>
               <div className="dropdown-content">
-                <a href="#">Fibroblast</a>
-                <a href="#">Hyaluron</a>
+                <Link href="/services/fibroblast"><a>Fibroblast</a></Link>
+                <Link href="/services/hyaluron"><a>Hyaluron</a></Link>
               </div>
             </li>
             <li className="dropdown">
-              <div className="dropbtn">Pricing&nbsp;<i className="fas fa-caret-down"></i></div>
+              <div className="dropbtn">Pricing&nbsp;<i aria-hidden className="fas fa-caret-down"></i><i aria-hidden className="fas fa-caret-up"></i></div>
               <div className="dropdown-content">
-                <a href="#">Fibroblast</a>
-                <a href="#">Hyaluron</a>
+                <Link href="/pricing/fibroblast"><a>Fibroblast</a></Link>
+                <Link href="/pricing/hyaluron"><a>Hyaluron</a></Link>
               </div>
             </li>
-            <li><a href="#news">Specials</a></li>
-            <li><a href="#news">Contact</a></li>
+            <li><Link href="/specials"><a>Specials</a></Link></li>
+            <li><Link href="/contact"><a>Contact</a></Link></li>
           </ul>
         </div>
       </header>
-      <main></main>
+      <main>
+        <div className="section">Summary</div>
+        <div className="section">Examples</div>
+        <div className="examples">
+          <div className="examples__fibroblast">
+            <div className="examples__fibroblast--image"></div>
+            <div className="examples__fibroblast--title">
+              <div>
+                Fibroblast Plasma
+              </div>
+              <div>
+                - See examples -
+              </div>
+            </div>
+          </div>
+          <div className="examples__hyaluron">
+            <div className="examples__hyaluron--image"></div>
+            <div className="examples__hyaluron--title">
+              <div>
+                Hyaluron Lip Treatment
+              </div>
+              <div>
+                - See examples -
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
       <footer></footer>
     </div>
   )
